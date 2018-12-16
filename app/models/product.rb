284e70b4ecbf3cefcd6071fe29804698b15f5a6d
danilomr <danilomelorocha@gmail.com>
2018-12-16 21:29:58 +0100

    Add function to retrieve counts of objects with missing fields

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  scope :missing_price, -> { where(price: nil) }
+  scope :missing_description, -> { where(description: nil) }
   validates :number, presence: true
   validates :name, presence: true
 end
