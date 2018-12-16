fd07a6c9f794b1fa7b0a579aa3bf0058e09a330d
danilomr <danilomelorocha@gmail.com>
2018-12-16 12:10:09 +0100

    Create model for Product, with fields from specification

---
+++ db/migrate/20181216105522_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.integer :number
+      t.string :name
+      t.float :price
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
